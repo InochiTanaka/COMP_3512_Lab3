1703e371c404510d4dbcd982fca01706b91e94e7
InochiTanaka <itanaka2@my.bcit.ca>
2018-02-03 00:34:04 -0800

    Impliment calling matrixTester

--- COMP_3512_Lab3/Matrixtester.cpp
+++ COMP_3512_Lab3/Matrixtester.cpp
@@ -1,14 +1,15 @@
 #include "Matrix.hpp"
-
-void matrixTester() {
-	
+void Matrix::matrixTester()
+{
 	int inputNum;
 
-	cout << "Input number for making Matrix" << endl;
+	cout << "Input number for making Matrix : ";
 	cin >> inputNum;
+	cout << endl;
 
 	cout << "Test #1: initialize matrix to square 1 x 1 contain 0" << endl;
 	Matrix mt;
 	cout << mt << endl;
 
-};
+
+}
