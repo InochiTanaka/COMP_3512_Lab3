bc636a58195b94525d0e5d1aff99c24adf402b40
InochiTanaka <itanaka2@my.bcit.ca>
2018-02-02 23:06:09 -0800

    impliment all functions, start debug and tester

--- COMP_3512_Lab3/Matrixtester.cpp
+++ COMP_3512_Lab3/Matrixtester.cpp
@@ -1,1 +1,14 @@
-//#include "Matrix.hpp"
+#include "Matrix.hpp"
+
+void matrixTester() {
+	
+	int inputNum;
+
+	cout << "Input number for making Matrix" << endl;
+	cin >> inputNum;
+
+	cout << "Test #1: initialize matrix to square 1 x 1 contain 0" << endl;
+	Matrix mt;
+	cout << mt << endl;
+
+};
